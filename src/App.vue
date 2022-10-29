c71e87d8b308a8222c5b0eba323e68d6a5b3e12d
astral <1723250309@qq.com>
2022-10-29 13:54:26 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+
+import Login from "./components/Login.vue";
+import Sign from "./components/Sign.vue";
+import {ref} from 'vue'
+
+const activeName = ref('first')
+
+</script>
+
+<template >
+  <el-tabs v-model="activeName" class="tabs">
+    <el-tab-pane class="ele" label="LOGIN" name="first">
+      <Login/>
+    </el-tab-pane>
+    <el-tab-pane class="ele" label="SIGN UP" name="second">
+      <Sign/>
+    </el-tab-pane>
+  </el-tabs>
+</template>
+
+<style scoped>
+.tabs {
+  margin-top: -120px;
+}
+
+.ele {
+  padding: 10px;
+}
+</style>
---
+++ src/components/Login.vue
@@ -0,0 +1,55 @@
+<script setup>
+import {ref} from 'vue'
+
+const user = ref('')
+const password = ref('')
+</script>
+
+<template>
+  <el-card class="box-card">
+    <template #header>
+      <div class="card-header">
+        <span>Hello</span>
+      </div>
+    </template>
+
+    <el-row>
+      <el-input v-model="user" placeholder="用户名"/>
+    </el-row>
+    <el-row>
+      <el-col>
+        <el-input
+            v-model="password"
+            type="password"
+            placeholder="密码"
+            show-password
+        />
+      </el-col>
+    </el-row>
+    <el-divider/>
+    <div id="operate">
+      <el-button type="primary" size="large">登录</el-button>
+    </div>
+  </el-card>
+</template>
+
+<style scoped>
+.box-card {
+  width: 25vw;
+}
+
+.card-header {
+  font-size: xxx-large;
+  font-weight: bolder;
+  color: #409EFF;
+  justify-content: space-between;
+  align-items: center;
+}
+.el-row {
+  margin-bottom: 10px;
+}
+
+#operate {
+  text-align: right;
+}
+</style>
